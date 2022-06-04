ff8c7ecbce6491688473738538b434f0f6bec42a
Dan-source <dan.dluis.dl@gmail.com>
2022-06-03 21:39:53 -0300

    Get the currente user and add loading settings

--- lib/models/user/user_manager.dart
+++ lib/models/user/user_manager.dart
@@ -8,17 +8,42 @@ import 'package:ecommerce_app/helpers/firebase_erros.dart';
 
 class UserManager extends ChangeNotifier {
 
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   FirebaseAuth auth = FirebaseAuth.instance;
 
+  User user;
+
+  bool _loading = false;
+  bool get loading => _loading;
+  set loading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
   Future<void> signIn({required costum.User user, required Function onFail, required Function onSuccess }) async {
+    loading = true;
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
-      print(result);
+          user = result as costum.User;
       onSuccess();
     } on FirebaseException catch (e) {
       onFail(getErrorString(e.toString()));
     }
+    loading = false;
+  }
+
+  Future<void> _loadCurrentUser({required UserCredential user}) async {
+    this.user = user as User;
+    notifyListeners();
+  }
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
   }
 
 }
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -38,82 +38,92 @@ class LoginScreen extends StatelessWidget {
           margin: const EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: _formKey,
-            child: ListView(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(16),
-              children: <Widget> [
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'E-mail',
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  autocorrect: false,
-                  validator: (email) {
-                    if (email!.isEmpty) {
-                      return 'Campo obrigatório.';
-                    }
-                    if (!emailValid(email)) {
-                      return 'E-mail inválido';
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: passController,
-                  decoration: const InputDecoration(
-                    labelText: 'Senha',
-                  ),
-                  obscureText: true,
-                  autocorrect: false,
-                  validator: (password) {
-                    if (password!.isEmpty || password.length < 6) {
-                      return 'Senha inválida';
-                    }
-                    return null;
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: TextButton(
-                    child: const Text('Esqueci minha senha'),
-                    style: TextButton.styleFrom(
-                      padding: EdgeInsets.zero,
-                      primary: Theme.of(context).primaryColor,
+            child: Consumer<UserManager>(
+              builder: (_, UserManager userManager, __) {
+
+                return ListView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.all(16),
+                  children: <Widget> [
+                    TextFormField(
+                      controller: emailController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(
+                        labelText: 'E-mail',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if (email!.isEmpty) {
+                          return 'Campo obrigatório.';
+                        }
+                        if (!emailValid(email)) {
+                          return 'E-mail inválido';
+                        }
+                        return null;
+                      },
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed('/forgot-password');
-                    },
-                  ),
-                ),
-                const SizedBox(height: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: Theme.of(context).primaryColor,
-                  ),
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      final user = costum.User(
-                        email: emailController.text,
-                        password: passController.text,
-                      );
-                      context.read<UserManager>().signIn(
-                        user: user,
-                        onFail: (e) {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text('Falha ao entrar: $e'),
-                            backgroundColor: Colors.red,
-                          ));
+                    TextFormField(
+                      controller: passController,
+                      enabled: !userManager.loading,
+                      decoration: const InputDecoration(
+                        labelText: 'Senha',
+                      ),
+                      obscureText: true,
+                      autocorrect: false,
+                      validator: (password) {
+                        if (password!.isEmpty || password.length < 6) {
+                          return 'Senha inválida';
+                        }
+                        return null;
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        child: const Text('Esqueci minha senha'),
+                        style: TextButton.styleFrom(
+                          padding: EdgeInsets.zero,
+                          primary: Theme.of(context).primaryColor,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pushNamed('/forgot-password');
                         },
-                        onSuccess: () {
-                          print("sucesso");
-                        });
-                    }
-                  },
-                  child: const Text('Entrar'),
-                ),
-              ]
-            ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).primaryColor,
+                      ),
+                      onPressed: userManager.loading ? null : () {
+                        if (_formKey.currentState!.validate()) {
+                          final user = costum.User(
+                            email: emailController.text,
+                            password: passController.text,
+                          );
+                          context.read<UserManager>().signIn(
+                            user: user,
+                            onFail: (e) {
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: Text('Falha ao entrar: $e'),
+                                backgroundColor: Color.fromARGB(255, 250, 110, 100),
+                              ));
+                            },
+                            onSuccess: () {
+                              print("sucesso");
+                            });
+                        }
+                      },
+                      child: userManager.loading
+                        ? const CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(Colors.white),
+                        ) : const Text('Entrar'),
+                    ),
+                  ]
+                );
+              },
+            )
           ),
         ),
       ),
